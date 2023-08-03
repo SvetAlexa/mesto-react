49b73a2cba1f02695e6177731c67a1d4850b4391
SvetAlexa <dazzle@bk.ru>
2023-08-03 23:07:35 +0300

    добавить стейт-переменные пользователя и обработчики изменений инпутов

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -1,19 +1,40 @@
-import PopupWithForm from "./PopupWithForm"
+import { useState, useContext, useEffect } from "react";
+import PopupWithForm from "./PopupWithForm";
+import { CurrentUserContext } from '../contexts/CurrentUserContext'
 
 export default function EditProfilePopup({ isOpen, onClose }) {
+    const currentUser = useContext(CurrentUserContext);
 
+    const [name, setName] = useState('');
+    const [about, setDescription] = useState('');
+    
+    console.log(currentUser.name)
+    console.log(currentUser.about)
+
+    useEffect(() => {
+        setName(currentUser.name);
+        setDescription(currentUser.about)
+    }, [currentUser])
+
+    function handleChangeName(evt) {
+        setName(evt.target.value);
+    }
+
+    function handleChangeDescription(evt) {
+        setDescription(evt.target.value);
+    }
 
     return (
         <PopupWithForm name="edit" title="Редактировать профиль" buttonText="Сохранить" isOpen={isOpen} onClose={onClose}>
             <ul className="popup__input-list">
                 <li className="popup__input-item">
                     <input type="text" name="user" placeholder="Ваше имя" minLength="2" maxLength="40" required
-                        className="popup__input popup__input_value_name" />
+                        className="popup__input popup__input_value_name" value={name} onChange={handleChangeName} />
                     <span className="error" id="user-error"></span>
                 </li>
                 <li>
                     <input type="text" name="about" placeholder="Ваше занятие" minLength="2" maxLength="200"
-                        required className="popup__input popup__input_value_activity" />
+                        required className="popup__input popup__input_value_activity" value={about} onChange={handleChangeDescription} />
                     <span className="error" id="about-error"></span>
                 </li>
             </ul>
