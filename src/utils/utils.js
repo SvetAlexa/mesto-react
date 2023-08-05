ae2cbd8d5b3cddb3069e089e72b00904296e4237
SvetAlexa <dazzle@bk.ru>
2023-08-06 02:45:41 +0300

    удалить лишний код в utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,3 @@
-// export const configFormSelector = {
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__input',
-//     submitButtonSelector: '.popup__button-sumbit',
-//     inactiveButtonClass: 'popup__button-sumbit_is-invalid',
-//     inputErrorClass: 'popup__input_is-invalid'
-// }
-
 export const config = {
     url: 'https://mesto.nomoreparties.co/v1/cohort-70',
     headers: {
